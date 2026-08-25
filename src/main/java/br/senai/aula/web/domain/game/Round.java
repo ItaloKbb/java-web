d3041bc9892c16912284aa0002afa630498b1ca6
Puchtaa <rafael.puchta@aluno.senai.br>
2026-08-25 16:44:54 -0300

    ajuste de conflito

--- src/main/java/br/senai/aula/web/domain/game/Round.java
+++ src/main/java/br/senai/aula/web/domain/game/Round.java
@@ -1,9 +1,12 @@
 package br.senai.aula.web.domain.game;
 
+enum statusRound{
     EM_ANDAMENTO, FINALIZADO
 }
 
+public record Round(Long id, Integer number, statusRound Status) {
 
+    public static Round newRound(Integer number, statusRound Status) {
 
         return new Round(null, number, Status);
     }
