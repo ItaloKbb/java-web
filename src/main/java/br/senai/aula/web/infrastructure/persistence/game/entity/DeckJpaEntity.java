be0726781ab08db0b8468cab328aa82e62925035
Rafael Puchta <rafael.puchta@aluno.senai.br>
2026-08-27 20:12:18 +0000

    Criada a entidade dos decks

---
+++ src/main/java/br/senai/aula/web/infrastructure/persistence/game/entity/DeckJpaEntity.java
@@ -0,0 +1,30 @@
+package br.senai.aula.web.infrastructure.persistence.user.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "deck")
+public class DeckJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    protected DeckJpaEntity() {
+    }
+
+    public DeckJpaEntity(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
